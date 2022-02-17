878f60ba964fa7ba731395e7b993b7ac3ca8b2b7
TheBouns <ilbonsi3@hotmail.com>
2022-02-17 17:06:09 +0100

    form v0.0.1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react"
+import './Form.css'
 export const Form = ()=>{
     const [data,setData]=useState({
         title: "",
@@ -22,25 +23,30 @@ const handleSubmit = (event)=>{
 }
     return(
         <div className="formContainer">
+            <h2>Public a NEW</h2>
+            <h3>Be free to share your local news with us.</h3>
             <form onSubmit={handleSubmit}>
+                <label>Set The Title</label>
                 <input 
                 type="text"
                 placeholder="Title"
                 onChange={inputChange}
                 value={data.name}
                 name="title"/>
+                <label>Set your Name</label>
                 <input
                 type="text"
                 placeholder= "Please enter your name"
                 onChange={inputChange}
                 value={data.author}
                 name="author"/>
-                <label>location</label>
+                <label>Location</label>
                 { <select name="location" onChange={inputChange} value={data.value}>
                     <option value="Valencia">Valencia</option>
                     <option value="Alicante">Alicante</option>
                     <option value="Castellon">Castellon</option>
                 </select>}
+                <label>Where,Who,When?</label>
                 <textarea
                 placeholder="Description"
                 onChange={inputChange}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom"
+import { FaNewspaper } from "react-icons/fa";
+
 import './Header.css'
 
  const Header = ()=>{
     return(
         <nav>
+            <div className="logo"><FaNewspaper/>SportsNews</div>
+            <div className="links">
             <p><Link to="/">Home</Link></p>
             <p><Link to="/form">Contact</Link></p>
             <p><Link to ="/news">News</Link></p>
+            </div>
+            
         </nav>
     );
 }
