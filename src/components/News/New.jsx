14b9cbf2162377fbff1be6c700fd400cf5d9ec89
Bonsi <amonselltapia@gmail.com>
2022-02-19 11:44:07 +0100

    card design finished

--- src/components/News/New.jsx
+++ src/components/News/New.jsx
@@ -1,26 +1,35 @@
-import React,{useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { GlobalContext } from "../../context/GlobalState";
-import './New.css'
+import { AiOutlineArrowRight } from "react-icons/ai";
+import "./New.css";
 
-const News =()=>{
-  const {news,getNews}   = useContext(GlobalContext);
-  useEffect(()=>{
-    getNews()
-  },);
-  const newItem= news.map((item)=>{
-    return(
-      
-      <div className="new" key={item.title} >
+const News = () => {
+  const { news, getNews } = useContext(GlobalContext);
+  useEffect(() => {
+    getNews();
+  });
+  const newItem = news.map((item) => {
+    if (item.title)
+      return (
+        <div className="new" key={item.title}>
           <img src={item.multimedia[1].url}></img>
-          <h2>{item.title}</h2>
-          <p>{item.abstract}</p>
-          <p>{item.byline}</p>
-          
-      </div>
-      
-    )
-  })
-  return <div className="newsContainer">{newItem}</div>
-}
+          <div className="newInfo">
+            <h2>{item.title}</h2>
+            <p>{item.abstract}</p>
+            <div className="newFooter">
+              <p>{item.byline}</p>
+              <a href={item.url} target="_blank">
+                <h3>
+                  Read More
+                  <AiOutlineArrowRight />
+                </h3>
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+  });
+  return <div className="newsContainer">{newItem}</div>;
+};
 
 export default News;
