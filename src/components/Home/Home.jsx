2440410ec06e4eae6b6a309ec5730859073bcff3
TheBouns <ilbonsi3@hotmail.com>
2022-02-21 17:39:52 +0100

    version 0.0.1 home css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
 import "./Home.css";
 const Home = () => {
-  const string = "in progress";
-  return <div className="homeContainer"></div>;
+  const sports = ["Football","Rugby","Basketball","Tennis","F1","Dakar","Ski","Olimpics","NCAA","Nascar","Golf","Karate","Judo","Swimming","Fishing","Kart","Dards","Soccer","Criket","Lacross","Padel","Bowling","Ping-Pong","Racing","Pool",]
+  const sport = sports.map((item,index)=>{
+    const string ="";
+    if(index%2==0){
+      return <strong>{item}-</strong>
+    }
+    else{
+      return <>{item}-</>
+    }
+  })
+  return( 
+  <div className="homeContainer">
+    <div className="sports">{sport}</div>
+    <div className="homeInfo">
+      <h1 className="homeTitle">Welcome To Sports News Website</h1>
+      <div className="linkToNews"><Link to="/news">NEWS!</Link></div>
+      
+    </div>
+  </div>
+  )
 };
 
 export default Home;
