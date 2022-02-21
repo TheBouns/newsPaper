5b926363ceecbdc5e7d374792fd3f8f8435b9865
Bonsi <amonselltapia@gmail.com>
2022-02-21 03:11:34 +0100

    form v.0.1.2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -22,49 +22,54 @@ export const Form = () => {
   };
   return (
     <div className="formContainer">
-      <div className="formImage">sdsdsd</div>
+      <div className="formImage">
+        <h2>Create a New</h2>
+        <p>Share your local news with us</p>
+      </div>
       <form onSubmit={handleSubmit}>
-        <label>Set The Title</label>
-        <input
-          type="text"
-          placeholder="Title"
-          onChange={inputChange}
-          value={data.name}
-          name="title"
-        />
-        <label>First Name</label>
-        <input
-          type="text"
-          placeholder="Please enter your name"
-          onChange={inputChange}
-          value={data.author}
-          name="firstname"
-        />
-        <label>Last Name</label>
-        <input
-          type="text"
-          placeholder="Please enter your name"
-          onChange={inputChange}
-          value={data.author}
-          name="lastname"
-        />
-        <label>Location</label>
-        {
-          <select name="location" onChange={inputChange} value={data.value}>
-            <option value="Valencia">Valencia</option>
-            <option value="Alicante">Alicante</option>
-            <option value="Castellon">Castellon</option>
-          </select>
-        }
-        <label>Where,Who,When?</label>
-        <textarea
-          placeholder="Description"
-          onChange={inputChange}
-          value={data.description}
-          name="description"
-        />
-
-        <button type="submit">Send</button>
+        <div className="insideForm">
+          <label> Title</label>
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={inputChange}
+            value={data.name}
+            name="title"
+          />
+          <label>First Name</label>
+          <input
+            type="text"
+            placeholder="Please enter your name"
+            onChange={inputChange}
+            value={data.author}
+            name="firstname"
+          />
+          <label>Last Name</label>
+          <input
+            type="text"
+            placeholder="Please enter your name"
+            onChange={inputChange}
+            value={data.author}
+            name="lastname"
+          />
+          <label>Location</label>
+          {
+            <select name="location" onChange={inputChange} value={data.value}>
+              <option value="Valencia">Valencia</option>
+              <option value="Alicante">Alicante</option>
+              <option value="Castellon">Castellon</option>
+            </select>
+          }
+          <label>Description</label>
+          <textarea
+            placeholder="Description"
+            onChange={inputChange}
+            value={data.description}
+            name="description"
+          />
+          <label>osdkos</label>
+          <button type="submit">Send</button>
+        </div>
       </form>
     </div>
   );
